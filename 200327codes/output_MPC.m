a6321c7ca1bf7f0d3dac0383d343099e9e9aa680
Wu <finne0831@gmail.com>
2020-03-27 22:51:20 +0800

    task

--- 200327codes/output_MPC.m
+++ 200327codes/output_MPC.m
@@ -20,34 +20,38 @@ C_f = eye(3);
 
 At = [0 1 -T_hw; 0 0 -1; 0 0 A_f];
 Bt = [0; 0; B_f];
-Ct = [1, 0, 0];
+Ct = [1, 1, 1];
 D = 0;
 Ts = 0.05;
 
 sys = ss(At, Bt, Ct, D, Ts);
-x0 = [-5; 1; 2]; %set something other than [0,0,0]
+x0 = [0; 2; 20]; %set initial state
 
 %% Design uncontrained LQR
-Qy = diag([1 0 0]);
+Qy = 1;
 R = 0.01;
 K_lqr = lqry(sys, Qy, R);
 
-% Run simulation
+%% Run simulation
 
-figure(1);
-t_unconstrained = 0:0.05:10;
-u_unconstrained = zeros(size(t_unconstrained));
-Unconstrained_LQR = tf([1,1,1])*feedback(ss(At, Bt, eye(3),0,Ts),K_lqr);
-lsim(Unconstrained_LQR, '-', u_unconstrained, t_unconstrained, x0);
-hold on;
-grid on;
+% figure(1);
+% t_unconstrained = 0:0.05:10;
+% u_unconstrained = zeros(size(t_unconstrained));
+% Unconstrained_LQR = tf([1,1,1])*feedback(ss(At, Bt, eye(3),0,Ts),K_lqr);
+% lsim(Unconstrained_LQR, '-', u_unconstrained, t_unconstrained, x0);
+% hold on;
+% grid on;
+% axis([0 10 -4 4]);
 
 
-%% 
+%% LQR with constraints
 
 x = x0;
 t_constrained = 0:40;
-for ct = t_constrained;
+uLQR = zeros(3,size(t_constrained, 2));
+yLQR = zeros(1,size(t_constrained, 2));
+
+for ct = t_constrained
     uLQR(ct+1) = -K_lqr*x;
     uLQR(ct+1) = max(-3,min(5,uLQR(ct+1)));
     x = At*x + Bt*uLQR(ct+1);
@@ -55,11 +59,14 @@ for ct = t_constrained;
 end
 figure(2);
 subplot(2,1,1);
-plot(t_constrained,uLQR);
+plot(t_constrained, uLQR);
 xlabel('time');
 ylabel('u');
+grid on;
+
 subplot(2,1,2);
-plot(t_constrained,yLQR);
+plot(t_constrained, yLQR);
 xlabel('time');
 ylabel('y');
 legend('Constrained LQR');
+grid on;
